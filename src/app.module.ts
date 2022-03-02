a39c949fa1e0535c1b349f8d15d534fe52f3e33d
Leandro Dantas <leandrosuy@gmail.com>
2022-03-02 12:26:00 -0300

    Subindo documentação da api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://leandrosuy:12345672@backendcarlos.ardio.mongodb.net/test'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://leandrosuy:12345672@backendcarlos.ardio.mongodb.net/usuarios'), UsersModule],
   controllers: [],
   providers: [],
 })
