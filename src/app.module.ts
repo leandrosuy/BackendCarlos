be95a7119807e864396cd5dcdcd0ee44f7b33907
Leandro Dantas <leandrosuy@gmail.com>
2022-03-02 11:47:27 -0300

    Subindo backend

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb+srv://leandrosuy:12345672@backendcarlos.ardio.mongodb.net/test'), UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
